02fcc8cac1c9f8cc06aaafd2ae07dce701f04618
anton popov <lankastersky@gmail.com>
2017-11-02 12:20:21 -0700

    startup script comments

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Based on https://github.com/GoogleCloudPlatform/nodejs-getting-started/tree/master/7-gce
 # [START startup]
 set -v
 
